8d4188cf36ef94eb0a96479886fa9fcb90e3f4e3
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-05 17:32:28 -0400

    structure complete

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,3 +1,21 @@
-public class Person {
+import java.time.LocalDate;
 
+public class Person {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+        return 0;
+    }
+
+    public void printPerson() {
+        // ...
+    }
 }
